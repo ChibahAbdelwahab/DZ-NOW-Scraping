0fad7ef199b4d0a7fa1adf978b59da8771f2e713
AbdelwahabChibah <abdelwahab.chibah@dataimpact.fr>
2019-08-31 22:31:44 +0100

    added category

--- dznow/spiders/TSA_SPIDER.py
+++ dznow/spiders/TSA_SPIDER.py
@@ -27,8 +27,7 @@ class TsaSpiderSpider(scrapy.Spider):
                 ".article-preview__desc ::text").get().strip()
             item["link"] = i.css("h1 a::attr(href)").get()
             item["title"] = i.css("h1 a::text").get()
-            # item["category"] = response
-            print(response.url.split("/")[-2:-1][0])
+            item["category"] = response.url.split("/")[-2:-1][0]
             item["image"] = i.css("a ::attr(data-bg)").get()
             yield Request(item['link'], self.parse_item, meta={"item": item})
 
