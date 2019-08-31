3318563f43e539b9a27d9f3bf0aa3839c8fb2cdc
AbdelwahabChibah <abdelwahab.chibah@dataimpact.fr>
2019-08-31 12:45:17 +0100

    spider ALgerie360  + liberte

---
+++ dznow/spiders/ALGERIE360_SPIDER.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+from dznow.items import NewsItem
+import datetime
+
+
+class Algerie360SpiderSpider(scrapy.Spider):
+    name = 'ALGERIE360_SPIDER'
+    allowed_domains = ['https://www.algerie360.com/rss-feedburner/']
+    start_urls = ['https://www.algerie360.com/rss-feedburner//']
+
+    def parse(self, response):
+        for i in response.css(".contentdiv"):
+            item = NewsItem()
+            item["title"] = i.css("a::attr('title')").get()
+            item["link"] = i.css("a::attr('href')").get()
+            # item["content"] = i.css("a::text").get()
+            # item["resume"] = i.css("a::text").get()
+            item["date"] = datetime.date.today()
+            item["category"] = 'Actualité'
+            yield item
---
+++ dznow/spiders/LIBERTE_SPIDER.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import datetime
+
+import scrapy
+from scrapy import Request
+
+from dznow.items import NewsItem
+
+
+class LiberteSpiderSpider(scrapy.spiders.XMLFeedSpider):
+    name = 'LIBERTE_SPIDER'
+    start_urls = ['https://www.liberte-algerie.com/article/feed/']
+    itertag = 'item'
+    custom_settings = {
+        "HTTPCACHE_ENABLED": 'True'
+    }
+
+    def parse_node(self, response, node):
+        item = NewsItem()
+        item['title'] = node.xpath('title/text()').get()
+        item['link'] = node.xpath('link/text()').get()
+        # item['content'] = node.xpath('description/text()').get()
+        item['resume'] = node.xpath('description/text()').get()
+        item["date"] = datetime.datetime.strptime(
+            node.xpath("pubDate/text()").get(), '%a, %d %b %Y %X +%f')
+        yield Request(item["link"], self.parse_item, meta={"item": item})
+
+    def parse_item(self, response):
+        item = response.meta["item"]
+        item["content"] = "".join(
+            str(x) for x in response.css("#text_core p::text").getall()
+        )
+        item["category"] = response.css("#global strong::text").get().strip()
+        # yield item
+        item["image"] = response.css(".post-image ::attr(src)").get()
+        print(response.url, item["image"])
